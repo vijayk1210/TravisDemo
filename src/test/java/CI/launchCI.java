b9aa662c8254bbaa6a0c66d3ee43c64770b6a18f
Vijay.Kumar <vijay.kumar@webvirtue.com>
2017-03-09 17:09:20 +0530

    first commit

---
+++ src/test/java/CI/launchCI.java
@@ -0,0 +1,17 @@
+package CI;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class launchCI {
+  @Test
+  public void travisTesting() {
+	    System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://qa1.homesofa.de/");
+		driver.manage().window().maximize();
+	  
+  }
+}
