af5bf2230e2889e6894a27ff7142ef61b3d3646c
Vijay.Kumar <vijay.kumar@webvirtue.com>
2017-03-09 17:59:41 +0530

    committed

--- src/test/java/CI/launchCI.java
+++ src/test/java/CI/launchCI.java
@@ -2,13 +2,14 @@ package CI;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class launchCI {
   @Test
   public void travisTesting() {
-	    System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+	    //System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver = new FirefoxDriver();
 		
 		driver.get("http://qa1.homesofa.de/");
 		driver.manage().window().maximize();
